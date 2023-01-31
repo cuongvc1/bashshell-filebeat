b82e3d687ec1d170d74b3db01b09e1a6b8754480
cuongvc1 <119566836+cuongvc1@users.noreply.github.com>
2023-01-31 21:05:23 +0700

    Update filebeat.sh

--- filebeat.sh
+++ filebeat.sh
@@ -71,7 +71,8 @@ for ((i=1;i<=n;i++));do
 	echo -n "paths[$i]= "
 	read m
 	a[$i]=$m
-	sed -i "/paths:$/a\\ - $m" /etc/filebeat/filebeat.yml
+	sed -i "/paths:$/a\
+	\  - $m" /etc/filebeat/filebeat.yml
 	echo -e " "
 done
 
