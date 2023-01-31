26c9721f4fd0b576ec7148c28f7bae6ca1503d9f
cuongvc1 <119566836+cuongvc1@users.noreply.github.com>
2023-01-31 20:25:15 +0700

    Update filebeat.sh

--- filebeat.sh
+++ filebeat.sh
@@ -30,9 +30,9 @@ fi
 cat <<END >/etc/filebeat/filebeat.yml
 filebeat.inputs:
 - enabled: true
-paths:
-tags: `hostname`
-type: log
+  paths:
+  tags: `hostname`
+  type: log
 output.logstash:
   hosts: 
   - 172.25.210.213:5033
