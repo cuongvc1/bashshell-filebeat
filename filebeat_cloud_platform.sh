333f293a79db27cb280b9ade9ad9e5e7115249ac
cuongvc1 <119566836+cuongvc1@users.noreply.github.com>
2023-02-08 14:33:16 +0700

    Update filebeat_cloud_platform.sh

--- filebeat_cloud_platform.sh
+++ filebeat_cloud_platform.sh
@@ -123,8 +123,8 @@ cat <<END >/etc/filebeat/filebeat.yml
 filebeat.inputs:
 - enabled: true
   paths:
-  - /var/log/syslog
-  - /var/log/auth.log
+  - /var/log/messages
+  - /var/log/secure
   tags: cloud_platform
   type: log
 output.logstash:
