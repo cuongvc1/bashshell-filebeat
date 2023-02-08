be8d10e4b455549f3bfcc7d32b443deb53f20253
cuongvc1 <119566836+cuongvc1@users.noreply.github.com>
2023-02-08 11:39:06 +0700

    Update install_filebeat.sh

--- install_filebeat.sh
+++ install_filebeat.sh
@@ -6,17 +6,17 @@ read -e -p 'Please choose for index_name, 1 or 2 or 3. [E.x. infrastructure || c
 
 if [ $team == 1 ]
 then
-    curl -o filebeat_security_system.sh https://raw.githubusercontent.com/cuongvc1/bashshell-filebeat/main/filebeat_cloud_service.sh
+    curl -o filebeat_security_system.sh https://raw.githubusercontent.com/cuongvc1/bashshell-filebeat/main/filebeat_security_system.sh
     bash filebeat_security_system.sh
 else
 if [ $team == 2 ]
 then
-    curl -o filebeat_cloud_platform.sh https://raw.githubusercontent.com/cuongvc1/bashshell-filebeat/main/filebeat_cloud_platform.sh 
+    curl -o filebeat_cloud_platform.sh https://raw.githubusercontent.com/cuongvc1/bashshell-filebeat/main/filebeat_cloud_platform.sh
     bash filebeat_cloud_platform.sh 
 else
 if [ $team == 3 ]
 then
-    curl -o filebeat_cloud_service.sh https://raw.githubusercontent.com/cuongvc1/bashshell-filebeat/main/filebeat_cloud_service.sh 
+    curl -o filebeat_cloud_service.sh https://raw.githubusercontent.com/cuongvc1/bashshell-filebeat/main/filebeat_cloud_service.sh
     bash filebeat_cloud_service.sh
 else
     exit 0
