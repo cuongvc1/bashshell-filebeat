d8f58fac49f8770c8262fa987c6cdd6b43569eb0
cuongvc1 <119566836+cuongvc1@users.noreply.github.com>
2023-02-12 15:06:44 +0700

    Update filebeat_security_system.sh

--- filebeat_security_system.sh
+++ filebeat_security_system.sh
@@ -124,14 +124,14 @@ then
         
         touch /etc/yum.repos.d/elastic.repo
 cat <<END >/etc/yum.repos.d/elastic.repo
-        [elastic-8.x]
-        name=Elastic repository for 8.x packages
-        baseurl=https://artifacts.elastic.co/packages/8.x/yum
-        gpgcheck=1
-        gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
-        enabled=1
-        autorefresh=1
-        type=rpm-md
+[elastic-8.x]
+name=Elastic repository for 8.x packages
+baseurl=https://artifacts.elastic.co/packages/8.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
 END
         rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
         yum install filebeat
